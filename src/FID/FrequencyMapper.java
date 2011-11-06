18a28324b7611dbdfba0bbfc3d2ee04aaaaf235c
Viswanath S <viswanathgs@gmail.com>
2011-11-06 19:12:13 +0530

    Parsers input logs to system call frequencies

---
+++ src/FID/FrequencyMapper.java
@@ -0,0 +1,95 @@
+package FID;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import Jama.Matrix;
+
+public class FrequencyMapper {
+	Map<Integer, Vector<Integer> > frequencyTable;
+	
+	// Maximum number of system calls in Linux kernel is hard-coded
+	public final int maxSystemCalls = 250;
+	
+	public FrequencyMapper(String processLog) throws IOException {
+		BufferedReader processLogFin = new BufferedReader(new FileReader(processLog));
+		frequencyTable = new HashMap<Integer, Vector<Integer> >();
+		
+		// Create the table of count of each system call for each process
+		String line;
+		StringTokenizer lineTokens;
+		while ((line = processLogFin.readLine()) != null) {
+			lineTokens = new StringTokenizer(line);
+			
+			// Each line should be <PID> <System Call ID>
+			if (lineTokens.countTokens() != 2) {
+				throw new IOException("Unknown data format. Expected format: <PID> <System call ID>");
+			}
+			
+			int pID = Integer.parseInt(lineTokens.nextToken());
+			int systemCallID = Integer.parseInt(lineTokens.nextToken());
+			
+			// System call ID should be in the range [1..maxSystemCalls]
+			if (systemCallID < 1 || systemCallID > maxSystemCalls) {
+				throw new IOException("Invalid system call ID");
+			}
+			
+			if (!frequencyTable.containsKey(pID)) {
+				Vector<Integer> frequencyList = new Vector<Integer>(maxSystemCalls);
+				for (int i = 0; i < maxSystemCalls; i++) {
+					frequencyList.set(i, 0);
+				}
+				
+				frequencyTable.put(pID, frequencyList);
+			}
+			
+			Vector<Integer> systemCallFrequency = frequencyTable.get(pID);
+			systemCallFrequency.set(systemCallID, systemCallFrequency.get(systemCallID) + 1);
+		}
+	}
+	
+	public Matrix getX() {
+		int n = maxSystemCalls;
+		int m = frequencyTable.size();
+		Matrix X = new Matrix(n, m);
+		
+		// Create matrix X from frequencyTable
+		Iterator<Entry<Integer, Vector<Integer> > > iter = frequencyTable.entrySet().iterator();
+		int j = 0;
+		while (iter.hasNext()) {
+			Map.Entry<Integer, Vector<Integer> > pairs = (Entry<Integer, Vector<Integer>>) iter.next();
+			Vector<Integer> systemCallFrequency = pairs.getValue();
+			
+			double totalFrequency = 0.0;
+			for (int i = 0; i < maxSystemCalls; i++) {
+				totalFrequency += systemCallFrequency.get(i);
+			}
+			for (int i = 0; i < maxSystemCalls; i++) {
+				X.set(i, j, (double) systemCallFrequency.get(i) / totalFrequency); 
+			}
+			
+			j++;
+		}
+		
+		return X;
+	}
+	
+	Vector<Integer> getPIDList() {
+		Vector<Integer> pidList = new Vector<Integer>();
+		
+		Iterator<Entry<Integer, Vector<Integer> > > iter = frequencyTable.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry<Integer, Vector<Integer> > pairs = (Entry<Integer, Vector<Integer>>) iter.next();
+			pidList.add(pairs.getKey());
+		}
+		
+		return pidList;
+	}
+}
